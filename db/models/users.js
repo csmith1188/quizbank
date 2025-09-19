5587a7714b048181bf8346944f1e2f230e97b17d
aRobert <124544863+RobertFilms@users.noreply.github.com>
2025-09-19 14:21:44 -0400

    new table for user entry

---
+++ db/models/users.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        uid: {
+            type: DataTypes.INTEGER,
+            unique: true,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        fb_id: {
+            type: DataTypes.INTEGER,
+        },
+        username: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.TEXT,
+        },
+        salt: {
+            type: DataTypes.TEXT,
+        },
+        email: {
+            type: DataTypes.TEXT,
+        },
+    }, {
+        tableName: "users",
+        timestamps: false,
+    });
+
+    return User;
+};
