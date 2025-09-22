f260a2217d697e4252c066cbe0d4019da4926424
aRobert <124544863+RobertFilms@users.noreply.github.com>
2025-09-22 10:27:58 -0400

    fixy

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { sequelize, Course, Section, Unit, Task, Question } = require("./models");
+const { sequelize, Course, Section, Unit, Task, Question } = require("./db");
 
 async function seed() {
   try {
