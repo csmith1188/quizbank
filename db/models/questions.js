d37dd74795224765ccc4790fbb3db8cc18ee78a0
hayde26904 <haydenfrobenius750@gmail.com>
2025-10-15 11:56:55 -0700

    workin hard

--- db/models/questions.js
+++ db/models/questions.js
@@ -8,10 +8,13 @@ module.exports = (sequelize, DataTypes) => {
         index: {
             type: DataTypes.INTEGER,
         },
-        prompt: { type: DataTypes.TEXT, allowNull: false },
-        correct_answer: { type: DataTypes.TEXT },
-        correct_index: { type: DataTypes.INTEGER },
         ai: { type: DataTypes.BOOLEAN, defaultValue: false },
+        type: {
+            type: DataTypes.ENUM('multiple-choice', 'multiple-answer', 'true-false', 'open-ended', 'fill-in-the-blank'),
+        },
+        prompt: { type: DataTypes.TEXT, allowNull: false },
+        correct_answers: { type: DataTypes.TEXT },
+        correct_answer_indexes: { type: DataTypes.INTEGER },
         answers: { type: DataTypes.TEXT }, // JSON string
     }, {
         tableName: 'questions'
