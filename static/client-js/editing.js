4396f058a9a962c13f4d0a7860c2f2f7dc4b50d8
aRobert <124544863+RobertFilms@users.noreply.github.com>
2025-10-27 14:28:54 -0400

    Create editing.js

---
+++ static/client-js/editing.js
@@ -0,0 +1,12 @@
+// Dynamic editing system for courses, sections, units, and tasks
+
+const currentEditingPath = {
+    course: null,
+    section: null,
+    unit: null,
+    task: null,
+};
+let path
+let description
+let genprompt
+
