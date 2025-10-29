e5f7926ae1ffecd3e63bafcd2499685ddfd562ec
Mr. Smith <csmith@ytech.edu>
2025-10-29 07:30:34 -0400

    dot env patch

--- controllers/login/formbar.js
+++ controllers/login/formbar.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UserService = require('../../services/user-service');
 const createSession = require('../../middleware/create-session');
