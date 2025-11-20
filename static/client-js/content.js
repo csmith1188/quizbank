0b0b4d0917717add1fc723ff0c761b2eaf911039
Robert <124544863+RobertFilms@users.noreply.github.com>
2025-11-20 11:58:30 -0500

    Fixed question edit buttons loading when not needed

--- static/client-js/content.js
+++ static/client-js/content.js
@@ -323,8 +323,10 @@ function renderView(view, filter = "") {
           <span>${display}</span>
           <span class="item-number">${item.number || item.index || ""}</span>
         </button>`;
+        if (view !== "questions"){
         html += `<button class="edit-btn" data-id="${item.uid ?? item.id}" title="Edit">✎ Edit</button>`;
         html += `</li>`;
+        }
     });
     html += `</ul>`;
     area.innerHTML = html;
