1ab4045d6f54ee389f5d9dc719a1f84d927732b8
hayde26904 <haydenfrobenius750@gmail.com>
2025-12-08 11:58:36 -0500

    a day

--- middleware/resource.js
+++ middleware/resource.js
@@ -1,5 +1,6 @@
 const resourceService = require('../services/resource-service');
 const { shallow } = require("../util/scope-limit");
+const {parseStringifiedArraysInObject} = require("../util/misc")
 
 module.exports = async (req, res, next) => {
     try {
@@ -7,7 +8,12 @@ module.exports = async (req, res, next) => {
         const questionType = req.query.type;
         const pickAmount = req.query.pick;
         const data = await resourceService.getResource(resourcePath, pickAmount, questionType);
-        
+
+        // band aid fix later
+        if (Array.isArray(data) && typeof data[0].answers === 'string') {
+          data[0].answers = JSON.parse(data[0].answers);
+        }
+
         res.send(shallow(data));
 
       } catch (err) {
--- services/resource-service.js
+++ services/resource-service.js
@@ -1,6 +1,6 @@
 const { sequelize, User, Course, Section, Unit, Task, Question } = require("../db/db");
 const { shallow } = require("../util/scope-limit");
-const { getRandomItems } = require('../util/misc');
+const { getRandomItems, parseStringifiedArraysInObject } = require('../util/misc');
 
 const resourceTypeModelMap = new Map([
     ["course", Course],
@@ -385,6 +385,7 @@ async function entitiesInSameHierarchy(entities) {
 
 };
 
+
 async function resolveResource(type, uid) {
     const model = resourceTypeModelMap.get(type);
     if (!model) {
@@ -461,7 +462,7 @@ function resolveHierarchy(root, segments) {
         return data;
     }
 
-    segments.forEach(({ type, ids }) => {
+    segments.forEach(({ type, ids }, index) => {
         const collection = data[type + "s"];
         if (!collection) throw new Error(`Invalid resource type: ${type}`);
 
@@ -481,6 +482,8 @@ function resolveHierarchy(root, segments) {
 
     return data;
 }
+
+
 
 module.exports.getResource = async (path, pickAmount = null, questionType = null) => {
     const segments = parseResourcePath(path);
